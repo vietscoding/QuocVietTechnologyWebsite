7a159355633d85771ee73eadad83f55133dfada1
Viet Doan <viet18725@gmail.com>
2025-06-25 00:23:07 +0700

    thêm các icon vào các nav item

--- src/admin/navbar.php
+++ src/admin/navbar.php
@@ -3,19 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nav</title>
+    <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/admin_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <style>
         .nav-link {
             color: #000000;
-            
+            border-radius: 10px;
+        }
+
+        .nav-item {
+            margin-right: 5px;
         }
 
         .nav-link:hover {
-            background-color:rgb(77, 157, 28);
+            background-color:rgb(147, 193, 21);
             color: #ffffff;
             text-decoration: none;
+            transition: all 0.2s ease;
+            /* border-radius: 10px; */
         }
 
         .active {
@@ -32,17 +39,57 @@
         <div class="container justify-content-start" style="width:10%" >
             <a class="navbar-brand" href="dashboard.php">
                 <img src="../assets/images/new_quocvietlogo.png" alt="Avatar Logo" style="width:40px;" class="rounded-pill"> 
+                
             </a>
+        </div>
+        <div class="container-fluid" style="width:10%">
+            <h2>Admin</h2>
         </div>
         <div class="container-fluid justify-content-end">
             <ul class="nav justify-content-end" >
-                <li class="nav-item"><a class="nav-link <?= ($current_page == 'dashboard') ? 'active' : '' ?>" href="dashboard.php">Dashboard</a></li>
-                <li class="nav-item"><a class="nav-link <?= ($current_page == 'manage_products') ? 'active nav-link' : '' ?>" href="manage_products.php">Products</a></li>
-                <li class="nav-item"><a class="nav-link <?= ($current_page == 'manage_users') ? 'active nav-link' : '' ?>" href="manage_users.php">Users</a></li>
-                <li class="nav-item"><a class="nav-link <?= ($current_page == 'manage_orders') ? 'active nav-link' : '' ?>" href="manage_orders.php">Orders</a></li>
-                <li class="nav-item"><a class="nav-link <?= ($current_page == 'admin_profile') ? 'active nav-link' : '' ?>" href="admin_profile.php">Profile</a></li>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($current_page == 'dashboard') ? 'active' : '' ?>" href="dashboard.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16"><path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/><path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+                        </svg> Dashboard
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link <?= ($current_page == 'manage_products') ? 'active nav-link' : '' ?>" href="manage_products.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-seam-fill" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z"/>
+                        </svg> Products
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link <?= ($current_page == 'manage_users') ? 'active nav-link' : '' ?>" href="manage_users.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16"><path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+                        </svg> Users
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link <?= ($current_page == 'manage_orders') ? 'active nav-link' : '' ?>" href="manage_orders.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-ul" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                        </svg> Orders
+                    </a>
+                
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($current_page == 'admin_profile') ? 'active nav-link' : '' ?>" href="admin_profile.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                        </svg> Profile
+                    </a>
+                </li>
+                
             </ul>
+
         </div>
+
     </nav>
 
 
